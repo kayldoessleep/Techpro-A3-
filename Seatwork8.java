a7edb88a90687ad7b795fc52680869bfdbeabc5a
kayldoessleep <kylecaguioa6911@gmail.com>
2025-08-28 16:44:27 +0800

    Create Seatwork8.java

---
+++ Seatwork8.java
@@ -0,0 +1,25 @@
+public class Seatwork8 {
+	public static void main(String[] args) {
+		int grade = 3;
+		
+		switch(grade){
+		    case 1:
+		    System.out.println("Excellent");
+		    break;
+		    
+		    case 2:
+		    case  3:
+		    System.out.println("Welldone");
+		    
+		    break;
+		    case 4:
+		    System.out.println("You passed");
+		    
+		    case 5:
+		    System.out.println("Better try again");
+default:
+System.out.println("invalid grade");
+
+		}	    
+System.out.println("Your grade is" +  grade);
+		}
