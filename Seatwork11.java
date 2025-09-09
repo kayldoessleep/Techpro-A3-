897c5377147bb80413f688c622dd5276ed37fbcc
kayldoessleep <kylecaguioa6911@gmail.com>
2025-09-09 17:36:37 +0800

    Create Seatwork11.java

---
+++ Seatwork11.java
@@ -0,0 +1,11 @@
+public class Seatwork11 {
+	public static void main(String[] args) {
+		System.out.println("Seatworkelibin");
+		
+		int[][] myNumbers = { {1,4,2}, {3,6,8} };
+		myNumbers[1][2] = 9;
+		System.out.println(myNumbers[1][2]);
+	
+		
+	}
+}
