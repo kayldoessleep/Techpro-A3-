23ce2ed489f9cba06de01d429e40d6ccc211fa8d
kayldoessleep <kylecaguioa6911@gmail.com>
2025-11-21 15:12:21 +0800

    Add MainLauncher with main method to run Student

---
+++ 3RDQSEATWORK2part1.java/MainLauncher.java
@@ -0,0 +1,9 @@
+public class MainLauncher {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Rhodinz");
+        s.setAge(16);
+        s.setCourse("ICT");
+        s.displayInfo();
+    }
+}
