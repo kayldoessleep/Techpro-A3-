f311723880949a1a8b696e673c254b6af480de1f
kayldoessleep <kylecaguioa6911@gmail.com>
2025-11-21 15:19:23 +0800

    Rename main method to THIRDQSEATWORK1

--- THIRDQSEATWORK1.java
+++ THIRDQSEATWORK1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void THIRDQSEATWORK1(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("How many grades (1 to 5): ");
