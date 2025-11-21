8ae1d2aa7112c3a798063951c57c74a637868fdf
kayldoessleep <kylecaguioa6911@gmail.com>
2025-11-21 15:18:15 +0800

    Create THIRDSEMSEATWORK1.java

---
+++ THIRDSEMSEATWORK1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many grades (1 to 5): ");
+        int count = input.nextInt();
+
+        double total = 0;
+
+        for (int i = 1; i <= count; i++) {
+            System.out.print("Enter grade " + i + ": ");
+            double grade = input.nextDouble();
+            total += grade;
+        }
+
+        double average = total / count;
+        System.out.println("Average: " + average);
+
+        char marka;
+
+        if (average >= 90) {
+            marka = 'A';
+        } else if (average >= 80) {
+            marka = 'B';
+        } else if (average >= 70) {
+            marka = 'C';
+        } else if (average >= 60) {
+            marka = 'D';
+        } else {
+            marka = 'F';
+        }
+
+        System.out.println("Letter grade: " + marka);
+
+        input.close();
+    }
+}
+}
