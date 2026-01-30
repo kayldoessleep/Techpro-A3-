3dc4662e98708f4f3382d5bedf9af8a108356d2b
kayldoessleep <kylecaguioa6911@gmail.com>
2026-01-31 00:20:58 +1200

    Add methods to calculate area of square and rectangle

---
+++ seatwork3rdquarter/seatwoek4part2.java
@@ -0,0 +1,18 @@
+public class seatwoek4part2 {
+    public int calculateArea(int side) {
+        return side * side;
+    }
+
+    public double calculateArea(double length, double width) {
+        return length * width;
+    }
+
+    // Main class below
+    public static void main(String[] args) {
+        seatwoek4part2 calc = new seatwoek4part2();
+
+        int squareArea = calc.calculateArea(5);
+        double rectangleArea = calc.calculateArea(4.0, 6.0);
+
+        System.out.println("Square area: " + squareArea);
+        System.out.println("Rectangle area: " + rectangleArea); 
