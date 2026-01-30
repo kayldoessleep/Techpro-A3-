7734cb10b6aef082f45cb0182d66b48717ff8696
kayldoessleep <kylecaguioa6911@gmail.com>
2026-01-31 00:43:17 +1200

    Create seatwork7a.java

---
+++ seatwork3rdquarter/seatwork7/seatwork7a.java
@@ -0,0 +1,14 @@
+public class seatwork7a {
+	public static void main(String[] args) {
+		
+		//built in
+		try{
+		    int a = 69;
+		    int b = 0;
+		    int result = a/b; 
+		    System.out.println(result);
+		}catch (ArithmeticException e) {
+		    System.out.println("Error: di gagana");
+		}
+	}
+}
