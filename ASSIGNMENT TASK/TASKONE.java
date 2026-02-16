7676d8e4517eb1948167aa8724ae0d36fba8c112
kayldoessleep <kylecaguioa6911@gmail.com>
2026-02-16 19:00:48 +1200

    Rename class and update main method
    Renamed class from 'Main' to 'TASKONE' and updated main method signature.

--- ASSIGNMENT TASK/TASKONE.java
+++ ASSIGNMENT TASK/TASKONE.java
@@ -2,8 +2,8 @@
 //task 1
 import java.util.Scanner;
 
-public class Main {
-    public static void TASKONE(String[] args) {
+public class TASKONE {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double weight;
         double newWeight;
